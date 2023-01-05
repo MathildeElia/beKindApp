c153e85d1681aa0ba1574cb79b3d19f8fba19d3e
NickTahmasebi <nicktahmasebi@hotmail.com>
2023-01-05 15:24:27 +0100

    Template Screen Fixed

--- app/src/main/java/com/example/kind1/TemplateScreen.kt
+++ app/src/main/java/com/example/kind1/TemplateScreen.kt
@@ -1,30 +1,35 @@
 package com.example.kind1
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun TemplateScreen(navController: NavController){
-    Column(
-        Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.TopCenter)) {
-        Row(Modifier.align(Alignment.Start)){
-            Image(painter = painterResource(id = R.drawable.backbutton), contentDescription = null,
-                modifier = Modifier
-                    .clickable {
-                        //navController.navigate(Screen.KindFront.route)
-                    }
-                    .size(width = 50.dp, height = 30.dp)
-            )
-        }
-
+    Column {
+        val shape = RoundedCornerShape(12.dp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Image(painter = painterResource(id = R.drawable.backbutton), contentDescription = null,
+            modifier = Modifier
+                .clickable {
+                    navController.navigate(Screen.KindFront.route)
+                }
+                .size(width = 50.dp, height = 30.dp)
+        )
     }
 }
+
