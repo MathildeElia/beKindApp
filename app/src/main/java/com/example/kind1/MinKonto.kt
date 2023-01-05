a4b096e5d739d36a0c29de04eef5777b1020320a
NickTahmasebi <nicktahmasebi@hotmail.com>
2023-01-05 14:53:14 +0100

    WowDesignMasterNickWoooW

--- app/src/main/java/com/example/kind1/MinKonto.kt
+++ app/src/main/java/com/example/kind1/MinKonto.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun MinKonto(/*modifier: Modifier = Modifier, navController: NavController*/)
