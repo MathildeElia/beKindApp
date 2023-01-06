96c7e39361694b1c412a2f3082b56e38da9cccde
seburu <sebynoah@gmail.com>
2023-01-06 12:13:23 +0100

    Firebase upgrades?

--- app/src/main/java/com/example/kind1/ViewModel.kt
+++ app/src/main/java/com/example/kind1/ViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.kind1
 
+import android.content.ContentValues
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
 
 class Viewmodel: ViewModel() {
 
@@ -25,4 +31,32 @@ class Viewmodel: ViewModel() {
         }
         return true
     }
+
+
+    fun addToDatabase(user: String, pass: String, email: String) {
+        viewModelScope.launch {
+            val db = Firebase.firestore
+
+            val newUser = hashMapOf(
+                "username" to user,
+                "password" to pass,
+                "email" to email
+            )
+            db.collection("users")
+                .add(newUser)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(
+                        ContentValues.TAG,
+                        "DocumentSnapshot added with ID: ${documentReference.id}"
+                    )
+                }
+                .addOnFailureListener { e ->
+                    Log.w(ContentValues.TAG, "Error adding document", e)
+                }
+                .addOnCompleteListener {
+                    Log.d("Test", "Is success: ${it.isSuccessful}")
+                }
+        }
+    }
+
 }
