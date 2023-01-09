273ad9322575fd748c75e2468fd4b6822036a987
seburu <sebynoah@gmail.com>
2023-01-09 14:10:12 +0100

    small button fix

--- app/src/main/java/com/example/kind1/BygPortfølje.kt
+++ app/src/main/java/com/example/kind1/BygPortfølje.kt
@@ -31,19 +31,16 @@ fun BygPortfølje (modifier: Modifier = Modifier, navController: NavController){
             contentDescription = null,
             modifier = Modifier.fillMaxSize()
         )
+        Image(painter = painterResource(id = R.drawable.backbutton), contentDescription = null,
+            modifier = Modifier
+                .clickable {
+                    navController.navigate(Screen.Portfølje.route)
+                }
+                .size(width = 50.dp, height = 30.dp)
+        )
         Column (modifier = Modifier.verticalScroll(rememberScrollState())){
             val shape = RoundedCornerShape(12.dp)
             Spacer(modifier = Modifier.height(10.dp))
-            Image(painter = painterResource(id = R.drawable.backbutton), contentDescription = null,
-                modifier = Modifier
-                    .clickable {
-                        navController.navigate(Screen.Portfølje.route)
-                    }
-                    .size(width = 50.dp, height = 30.dp)
-                    .verticalScroll(rememberScrollState())
-                    .weight(weight = 1f, fill = false)
-
-            )
             Spacer(modifier = Modifier.height(10.dp))
             Text(
                 text = "\t\tByg din Portfølje",
