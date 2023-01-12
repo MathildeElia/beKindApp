504c69961834f7b369b50cea70c6a6cbea747ba9
Sofiedige <sofiedige1@gmail.com>
2023-01-12 16:20:01 +0100

    padding

--- app/src/main/java/com/example/kind1/view/PortføljeNY.kt
+++ app/src/main/java/com/example/kind1/view/PortføljeNY.kt
@@ -43,7 +43,7 @@ fun PortføljeNy(navController: NavController) {
             Image(painter = painterResource(id = R.drawable.backbutton), contentDescription = null,
                 modifier = Modifier
                     .clickable {
-                        navController.navigate(Screen.KindFront.route)
+                        navController.navigate(Screen.KindStart.withArgs("Gæst"))
                     }
                     .size(width = 50.dp, height = 30.dp)
             )
