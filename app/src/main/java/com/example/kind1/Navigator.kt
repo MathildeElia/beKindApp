1cb8e49637085642720016db0c8d2fa7ecc1707e
MathildeElia <91071893+MathildeElia@users.noreply.github.com>
2023-01-16 13:59:48 +0100

    Navigator startdesitnation.

--- app/src/main/java/com/example/kind1/Navigator.kt
+++ app/src/main/java/com/example/kind1/Navigator.kt
@@ -19,7 +19,7 @@ fun Navigation() {
 
         NavHost(
             navController = navController,
-            startDestination = Screen.Organisation.route
+            startDestination = Screen.KindFront.route
         ) {
             composable(route = Screen.KindFront.route) {
                 KindFront(navController = navController)
